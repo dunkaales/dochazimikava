c344d6fd1eeea96a6f9b6acdfefd64e9bfee0c80
dunkaales <148335822+dunkaales@users.noreply.github.com>
2023-10-18 17:46:16 +0200

    tailwind setup
    neumim!! :33

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//vubec nevim co to dela a co se tu deje ale to se fituje (snad)
+// strasne me sere jak nicemu nerozumim!!! >:(
