eec30228d5af4c57de099df7e2f1c61c7e442ac7
dunkaales <148335822+dunkaales@users.noreply.github.com>
2023-10-21 13:44:18 +0200

    mega ez shit to je

--- testscript.js
+++ testscript.js
@@ -1,120 +1,157 @@
 
-    const questions = [
-        {
-            question: "1. Do you feel anxious or stressed when you are without a phone or computer device for an extended period of time?",
-            answer: "Somehow"
-        },
-        {
-            question: "2. Does your use of a phone or computer device affect your social or love life?",
-            answer: "Somehow"
-        },
-        {
-            question: "3. Do you spend the majority of your time online?",
-            answer: "Somehow"
-        },
-        {
-            question: "4. Do you neglect your basic bodily needs, such as hygiene, sleep, or eating, in order to spend more time on your phone or computer device?",
-            answer: "Somehow"
-        },
-        {
-            question: "5. Do you primarily communicate through the internet?",
-            answer: "Somehow"
-        },
-        {
-            question: "6. Have you ever missed important real-life events or obligations because of excessive phone or computer usage?",
-            answer: "Somehow"
-        },
-        {
-            question: "7. Do you frequently lose track of time while using your phone or computer, leading to hours passing without your awareness?",
-            answer: "Somehow"
-        },
-        {
-            question: "8. Do you struggle with any addictions, such as alcohol abuse, drug abuse, or nicotine addiction?",
-            answer: "Somehow"
-        },
-        {
-            question: "9. Do you experience feelings of depression?",
-            answer: "Somehow"
-        },
-        {
-            question: "10. Do you consider yourself productive in any way?",
-            answer: "Somehow"
-        },
-        {
-            question: "11. Do you consume energy drinks or coffee solely to extend your time on your phone or computer device?",
-            answer: "Somehow"
-        },
-        {
-            question: "12. Do you hide the amount of time you spend online?",
-            answer: "Somehow"
-        },
-        {
-            question: "13. Have you ever considered seeking professional help or support to manage your phone or computer usage?",
-            answer: "Somehow"
-        }
-    ];
-    
-    let score = 0;
-    
-    const questionsElement = document.getElementById('questions');
-    const textPsych = document.getElementById('textpsych');
-    const scoreValueElement = document.getElementById('score-value');
-    
-    
-    questionsElement.innerHTML = "";
-    questions.forEach((q, index) => {
-        questionsElement.innerHTML += `
-            <div class="question-container">
-                <div>${q.question}</div>
-                <div class="answer-container">
-                    <div class="radio-label">
-                        <input type="radio" name="q${index}" value="Yes" id="q${index}_yes">
-                        <label for="q${index}_yes">Yes</label>
-                    </div>
-                    <div class="radio-label">
-                        <input type="radio" name="q${index}" value="Somehow" id="q${index}_somehow">
-                        <label for="q${index}_somehow">Somehow</label>
-                    </div>
-                    <div class="radio-label">
-                        <input type="radio" name="q${index}" value="No" id="q${index}_no">
-                        <label for="q${index}_no">No</label>
-                    </div>
+const questions = [
+    {
+        question: "1. Do you feel anxious or stressed when you are without a phone or computer device for an extended period of time?",
+        answer: "Somehow"
+    },
+    {
+        question: "2. Does your use of a phone or computer device affect your social or love life?",
+        answer: "Somehow"
+    },
+    {
+        question: "3. Do you spend the majority of your time online?",
+        answer: "Somehow"
+    },
+    {
+        question: "4. Do you neglect your basic bodily needs, such as hygiene, sleep, or eating, in order to spend more time on your phone or computer device?",
+        answer: "Somehow"
+    },
+    {
+        question: "5. Do you primarily communicate through the internet?",
+        answer: "Somehow"
+    },
+    {
+        question: "6. Have you ever missed important real-life events or obligations because of excessive phone or computer usage?",
+        answer: "Somehow"
+    },
+    {
+        question: "7. Do you frequently lose track of time while using your phone or computer, leading to hours passing without your awareness?",
+        answer: "Somehow"
+    },
+    {
+        question: "8. Do you struggle with any addictions, such as alcohol abuse, drug abuse, or nicotine addiction?",
+        answer: "Somehow"
+    },
+    {
+        question: "9. Do you experience feelings of depression?",
+        answer: "Somehow"
+    },
+    {
+        question: "10. Do you consider yourself productive in any way?",
+        answer: "Somehow"
+    },
+    {
+        question: "11. Do you consume energy drinks or coffee solely to extend your time on your phone or computer device?",
+        answer: "Somehow"
+    },
+    {
+        question: "12. Do you hide the amount of time you spend online?",
+        answer: "Somehow"
+    },
+    {
+        question: "13. Have you ever considered seeking professional help or support to manage your phone or computer usage?",
+        answer: "Somehow"
+    }
+];
+
+let score = 0;
+
+const questionsElement = document.getElementById('questions');
+const textPsych = document.getElementById('textpsych');
+const scoreValueElement = document.getElementById('score-value');
+
+
+questionsElement.innerHTML = "";
+questions.forEach((q, index) => {
+    questionsElement.innerHTML += `
+        <div class="question-container">
+            <div>${q.question}</div>
+            <div class="answer-container">
+                <div class="radio-label">
+                    <input type="radio" name="q${index}" value="Yes" id="q${index}_yes">
+                    <label for="q${index}_yes">Yes</label>
+                </div>
+                <div class="radio-label">
+                    <input type="radio" name="q${index}" value="Somehow" id="q${index}_somehow">
+                    <label for="q${index}_somehow">Somehow</label>
+                </div>
+                <div class="radio-label">
+                    <input type="radio" name="q${index}" value="No" id="q${index}_no">
+                    <label for="q${index}_no">No</label>
                 </div>
             </div>
-        `;
-    });
-    
-    const radioButtons = document.querySelectorAll('input[type="radio"]');
-    radioButtons.forEach((radio) => {
-        radio.addEventListener('change', () => {
-            calculateScore();
-        });
-    });
-    
-    function calculateScore() {
-        score = 0;
-        radioButtons.forEach((radio) => {
-            if (radio.checked) {
-                if (radio.value === "Yes") {
-                    score += 2;
-                } else if (radio.value === "Somehow") {
-                    score += 1;
-                }
-            }
-        });
-    }
-    function showResult(){
-    
-        scoreValueElement.textContent = score;
-        if (score > 17) {
-            textPsych.textContent = 'The score of your test implies that you are highly addicted to your phone/computer device & the internet.';
-        } else if (score > 9) {
-            textPsych.textContent = 'The score results reflect significant device usage, yet without signs of addiction.';
-        } else {
-            textPsych.textContent = 'The score results indicate a healthy and balanced approach to device usage, without any signs of addiction';
-        }
+        </div>
+    `;
+});
 
-        const resultButton = document.querySelector('button');
-        resultButton.style.display = 'none';
-        questionsElement.style.display = 'none';
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+radioButtons.forEach((radio) => {
+    radio.addEventListener('change', () => {
+        calculateScore();
+    });
+});
+
+function calculateScore() {
+    score = 0;
+    radioButtons.forEach((radio) => {
+        if (radio.checked) {
+            if (radio.value === "Yes") {
+                score += 2;
+            } else if (radio.value === "Somehow") {
+                score += 1;
+            }
+        }
+    });
+}
+function showResult(){
+
+    scoreValueElement.textContent = score;
+    if (score > 17) {
+        textPsych.textContent = 'The score of your test implies that you are highly addicted to your phone/computer device & the internet.';
+    } else if (score > 9) {
+        textPsych.textContent = 'The score results reflect significant device usage, yet without signs of addiction.';
+    } else {
+        textPsych.textContent = 'The score results indicate a healthy and balanced approach to device usage, without any signs of addiction';
     }
+
+    const resultButton = document.querySelector('button');
+    resultButton.style.display = 'none';
+    questionsElement.style.display = 'none';
+}
+
+        // Get all elements with the class "radio-container"
+        const radioContainers = document.querySelectorAll(".answer-container > div.radio-label");
+
+            // Add click event listeners to each radio container
+            radioContainers.forEach(container => {
+            container.addEventListener("click", function() {
+                // Find the associated radio input element and check it
+                const radioInput = this.querySelector('input[type="radio"]');
+                radioInput.checked = true;
+            });
+        });
+
+        function updateSelectedClass() {
+            radioContainers.forEach(container => {
+                const radioInput = container.querySelector('input[type="radio"]');
+                if (radioInput.checked) {
+                    container.classList.add("selected");
+                } else {
+                    container.classList.remove("selected");
+                }
+            });
+        }
+        
+        // Add click event listeners to each radio container
+        radioContainers.forEach(container => {
+            container.addEventListener("click", function(event) {
+                event.stopPropagation(); // Stop event propagation
+                const radioInput = this.querySelector('input[type="radio"]');
+                if (radioInput) {
+                    radioInput.checked = true;
+                    calculateScore(); // Recalculate the score when a radio input is checked
+                    updateSelectedClass(); // Update the "selected" class
+                }
+            });
+        });
